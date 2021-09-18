78122e7be6ba66bd84f7c187a321b9508fb9dc6e
Макс <yashanty@gmail.com>
2021-09-19 01:34:57 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LazyRepeater
+{
+    class Program
+    {
+        public static void Main()
+        {
+            static Func<char> MakeLooper(string str)
+            {
+                int i = -1;
+                /*Func<char> a = () =>
+                {
+                    i = (i < str.Length - 1) ? ++i : 0;
+                    return str[i];
+                };
+                return a;*/
+                Func<char> a = () =>
+                {
+                    i = (i < str.Length-1) ? ++i : 0;
+                    Console.WriteLine(str[i]);//вывод на консоль возвращаемого значения
+                    return str[i];
+                };
+                return a;
+
+            }
+
+
+            Func<char> abc = MakeLooper("abc");
+            abc();//a
+            abc();//b
+            abc();//c
+            abc();//a
+            abc();//b
+
+        }
+
+
+    }
+}
